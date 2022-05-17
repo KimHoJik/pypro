a3461decea982f162c7a7baf702d73fd7fa03cc0
KimHoJik <hodo25@naver.com>
2022-05-17 18:31:09 +0900

    5월 17일 수업내용

---
+++ pypro4/pack1/tf1.py
@@ -0,0 +1,42 @@
+# DeepLearning library : TensorFlow
+
+# 텐서플로우의 이해
+import tensorflow as tf
+print(tf.__version__)
+
+# 상수 정의
+print(1, type(1))
+print([1], type([1]))
+print(tf.constant(1), type(tf.constant(1)))  # scalar : 0_D tensor
+print(tf.constant([1]))  # vector : 1_D tensor
+print(tf.constant([[1]]))  # matrix : 2_D tensor
+
+print()
+a = tf.constant([1, 2])
+b = tf.constant([3, 4])
+c = a + b
+print(c, type(c))
+c = tf.add(a, b)
+print(c, type(c))
+# d = tf.constant([3])
+# d = tf.constant(3)
+d = tf.constant([[3]])
+e = c + d
+print(e)  # boradcast 연산
+
+print()
+print(7, type(7))
+print(tf.convert_to_tensor(7, dtype=tf.float32))
+print(tf.cast(7, dtype=tf.float32))
+print(tf.constant(7.0))
+print(tf.constant(7, dtype=tf.float32))
+print()
+import numpy as np
+arr = np.array([1, 2, 3])
+print(arr, type(arr))
+tfarr = tf.add(arr, 5)
+print(tfarr)
+print(tfarr.numpy())  # numpy type으로 형변환 (강제)
+print(np.add(tfarr, 3))  # numpy type으로 형변환 (자동)
+
+print(list(tfarr.numpy()))
